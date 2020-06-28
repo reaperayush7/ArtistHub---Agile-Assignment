d930bdca7758862e58530a2e13399ed7d9587865
PRAJWOL <prajwolbogati9@gmail.com>
2020-06-28 23:04:12 +0545

    event organizer registration

---
+++ Backend/routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var User = require('../models/Users');
+var passport = require('passport');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+//Users register and login
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User(req.body),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+      }
+      else {
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ success: true, status: 'Registration Successful!' });
+        });
+      }
+    });
+});
