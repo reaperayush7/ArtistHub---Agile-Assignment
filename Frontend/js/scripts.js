d0bca1ea45e8c76f1db83e324e59fbcf9249c8b5
unknown <sinoxcrest@gmail.com>
2020-07-03 15:04:43 +0545

    Artist view Event

--- Frontend/js/scripts.js
+++ Frontend/js/scripts.js
@@ -41,3 +41,28 @@ $(function () {
          });
      });
 
+     function rowTemplate(hero) {
+        let oneRow = '<div class="card-body">'+ ' <h6 class="card-title">Job name:&nbsp;'+hero.name + 
+        '</h6>' + '<p><small class="ml-1">job type:&nbsp; '+hero.JobType + '</p>'+
+        '<b>Preference Skills: </b>'+hero.Description+'<br><br><b>Total Applicant: </b>'+
+        hero.Applicant+'<p class="card-text mt-3"><b>Deadline: </b>'+hero.Deadline +
+        '<br><b>Salary: </b>'+hero.Salary+'</div>';
+        oneRow += '<button type="button" class="btn btn-danger Applyjob" data-toggle="modal" data-target="#modalLRForms" apply_id=' + hero._id + ' id="applyform">Apply job</button><hr>';
+      
+        return oneRow;
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: base_url + 'eventposts',
+        success: function (heroes) {
+            let myRows = [];
+            $.each(heroes, function (index, hero) {
+                myRows.push(rowTemplate(hero));
+            });
+            tblBody.append(myRows);
+        },
+        error: function () {
+            alert('Something went wrong!');
+        }
+    });
