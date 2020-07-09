f99c945ad2e90fd5e881dff665ce9a8860cdd50f
PRAJWOL <prajwolbogati9@gmail.com>
2020-07-09 11:03:46 +0545

    Artist  manage profile

--- Backend/routes/artists.js
+++ Backend/routes/artists.js
@@ -56,7 +56,80 @@ router.route('/:id')
         res.statusCode = 403;
         res.end("POST operation not supported!");
     })
-  
+    .put((req, res, next) => {
+        Artist.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true, useFindAndModify: false })
+            .then((Artist) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(Artist);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Artist.findById(req.params.id)
+            .then((Artist) => {
+                let path = './public/images/' + Artist.image;
+                fs.unlink(path, (err) => {
+                    if (err) console.log(err);
+                })
+                Artist.delete()
+                    .then((reply) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json(reply);
+                    })
+            }).catch((err) => next(err));
+
+            Artist.findByIdAndDelete(req.params.id)
+            .then((reply) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(reply);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+ // yaa dekhi comments ko suru hunx
+
+router.route('/:id/comments')
+.get((req, res, next) => {
+    Artist.findById(req.params.id)
+        .then((Artist) => {
+            if (Artist != null) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(Artist.comments);
+            }
+            else {
+                err = new Error('Hero ' + req.params.id + ' not found');
+                err.status = 404;
+                return next(err);
+            }
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    Artist.findById(req.params.id)
+        .then((Artist) => {
+            if (Artist != null) {
+                Artist.comments.push(req.body);
+                Artist.save()
+                    .then((Artist) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json(Artist);
+                    }, (err) => next(err));
+            } else {
+                err = new Error('Artist ' + req.params.id + ' not found');
+                err.status = 404;
+                return next(err);
+            }
+        }, (err) => next(err))
+        .catch((err) => next(err));
+});
+
+
+
 
 
 module.exports = router
